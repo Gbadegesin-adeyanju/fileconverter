13f272c0040149c83ae27d816a1f0991be8c9cca
Gbadegesin Adeyanju <153382045+Gbadegesin-adeyanju@users.noreply.github.com>
2026-05-20 15:51:25 +0100

    Delete converter_app directory

--- converter_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import EmailUsers, Newsletter
-
-
-# Register your models here.
-admin.site.register(EmailUsers)
-admin.site.register(Newsletter)
-
--- converter_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import EmailUsers
-
-class EmailUsersForm(forms.ModelForm):
-    class Meta:
-        model = EmailUsers
-        fields = ['email']
